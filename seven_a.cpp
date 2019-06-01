5c65b0405cf67f09048ea47c9bfdbe39152687c5
Harivansh Mathur <mathurharivansh811@gmail.com>
2019-06-01 11:50:58 +0530

    Added main fuction

--- seven_a.cpp
+++ seven_a.cpp
@@ -19,3 +19,27 @@ a[j]=a[j-1];
 a[j-1]=p;
 }
 }
+int main()
+{
+int s[5]={4,5,-8,0,87};
+float c[5]={1.4,5.7,-6.8,5.446,0.99};
+bubble_sort(s,5);
+bubble_sort(c,5);
+int x=0,y=0;
+cout<<"Integer array in ascending order:"<<endl;
+while(x<5)
+{
+cout<<s[x]<<"\n";
+x++;
+}
+
+
+cout<<"Floating array in ascending order:"<<endl;
+while(y<5)
+{
+cout<<c[y]<<"\n";
+y++;
+}
+return 0;
+}
+
